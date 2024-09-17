8d26495af37391efa916cb0090cf300fbad3d8ee
amhsm6 <amhsm6@gmail.com>
2024-09-17 20:22:31 +0300

    concurrent send updates to client

--- server/app/Clipboard.hs
+++ server/app/Clipboard.hs
@@ -7,12 +7,13 @@ module Clipboard where
 import Control.Monad
 import Control.Monad.Reader
 import Control.Lens
+import Control.Concurrent
 import Control.Concurrent.STM
-import Control.Exception (Exception, catch)
+import Control.Exception (Exception, SomeException, catch)
 import qualified Data.Text.Lazy as T
 import qualified Data.Map as M
 import Text.Printf
-import Network.WebSockets (Connection, sendTextData, receiveData, ConnectionException)
+import Network.WebSockets (Connection, sendTextData, receiveData, withPingPong, defaultPingPongOptions)
 
 type WorkerT = ReaderT State
 
@@ -29,6 +30,9 @@ runWorkerT = runReaderT
 
 atom :: WorkerT STM a -> WorkerT IO a
 atom m = ask >>= liftIO . atomically . runWorkerT m
+
+fork :: WorkerT IO () -> WorkerT IO ()
+fork m = ask >>= void . liftIO . forkIO . runWorkerT m
 
 get :: WorkerT STM Clipboard
 get = do
@@ -52,6 +56,11 @@ connect = do
 
     pure connId
 
+pingpong :: WorkerT IO () -> WorkerT IO ()
+pingpong m = do
+    (State l x c) <- ask
+    liftIO $ withPingPong defaultPingPongOptions c $ runWorkerT m . State l x
+
 send :: T.Text -> Connection -> WorkerT IO ()
 send msg conn = liftIO $ do
     sendTextData conn $ T.singleton 'A'
@@ -71,26 +80,35 @@ handle handler m = do
 work :: Int -> WorkerT IO ()
 work clipboardId = do
     connId <- atom connect
-    liftIO $ putStrLn $ printf "[%d]: connect: %d" clipboardId connId
+    liftIO $ putStrLn $ printf "[%d] connect: %d" clipboardId connId
 
-    join $ liftM2 send (atom $ view contents <$> get) me
-
-    let onClose :: ConnectionException -> WorkerT IO ()
+    let onClose :: SomeException -> WorkerT IO ()
         onClose _ = do
-            liftIO $ putStrLn $ printf "[%d]: disconnect: %d" clipboardId connId
+            liftIO $ putStrLn $ printf "[%d] disconnect: %d" clipboardId connId
             atom $ alter $ clients . at connId .~ Nothing
 
     handle onClose $ do
-        forever $ do
-            upd <- recv
-            atom $ alter $ contents .~ upd
+        pingpong $ do
+            join $ liftM2 send (atom $ view contents <$> get) me
 
-            liftIO $ putStrLn $ printf "[%d] FROM %d: %s" clipboardId connId $ T.take 1024 upd
+            forever $ do
+                upd <- recv
+                atom $ alter $ contents .~ upd
 
-            s <- atom get
-            iforMOf_ (clients . itraversed . ifiltered (const . (/=connId))) s $ \i c -> do
-                liftIO $ putStrLn $ printf "[%d] TO %d" clipboardId i
-                send upd c
+                liftIO $ putStrLn $ printf "[%d] FROM %d: %s" clipboardId connId $ T.take 1024 upd
 
-            liftIO $ putStrLn $ printf "[%d] SYNC %d" clipboardId connId
-            sync
+                tsent <- atom $ lift $ newTVar 0
+
+                s <- atom get
+                iforMOf_ (clients . itraversed . ifiltered (const . (/=connId))) s $ \i c -> do
+                    fork $ do
+                        liftIO $ putStrLn $ printf "[%d] TO %d" clipboardId i
+                        send upd c
+
+                        atom $ lift $ modifyTVar tsent (+1)
+
+                let msgs = length $ s ^. clients
+                atom $ lift $ readTVar tsent >>= check . (==msgs-1)
+
+                liftIO $ putStrLn $ printf "[%d] SYNC %d" clipboardId connId
+                sync
