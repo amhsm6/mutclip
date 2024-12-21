538c59522cb9ee2fc0fd82017102acca85a24df9
anton <amhsm6@gmail.com>
2024-12-21 13:57:46 +0200

    fix potential csrf

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
-    output: "standalone"
+    output: "standalone",
+    experimental: {
+        serverActions: {
+            allowedOrigins: ["34.227.150.51"]
+        }
+    }
 };
 
 export default nextConfig;
